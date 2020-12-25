0c0382cd2f618d207439b9419ba78893469725ba
Himadri Chakrabarti <himadric@yahoo.com>
2020-12-25 18:09:08 -0500

    Clean up and coumentation

--- resolvers/index.ts
+++ resolvers/index.ts
@@ -1,5 +1,4 @@
 import * as path from 'path'
-//import { fileLoader } from 'merge-graphql-schemas'
 import { loadFilesSync } from '@graphql-tools/load-files';
 
 const resolvers = loadFilesSync(path.join(__dirname, './**/*.resolvers.*'))
--- typedefs/index.ts
+++ typedefs/index.ts
@@ -1,5 +1,4 @@
 import * as path from "path";
-//import { fileLoader, mergeTypes } from "merge-graphql-schemas";
 import { mergeTypeDefs } from '@graphql-tools/merge';
 import { loadFilesSync } from '@graphql-tools/load-files';
 
