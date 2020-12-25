98c9961c37c4b886ebd202e691223e7fbdbd0854
Himadri Chakrabarti <himadric@yahoo.com>
2020-12-25 14:13:48 -0500

    Migrated code to graphql-tools

--- typedefs/index.ts
+++ typedefs/index.ts
@@ -1,7 +1,9 @@
 import * as path from "path";
-import { fileLoader, mergeTypes } from "merge-graphql-schemas";
+//import { fileLoader, mergeTypes } from "merge-graphql-schemas";
+import { mergeTypeDefs } from '@graphql-tools/merge';
+import { loadFilesSync } from '@graphql-tools/load-files';
 
-const typesArray = fileLoader(path.join(__dirname, "./"));
-const typesMerged = mergeTypes(typesArray);
+const typesArray = loadFilesSync(path.join(__dirname, "./"));
+const typesMerged = mergeTypeDefs(typesArray);
 
 export default typesMerged;
